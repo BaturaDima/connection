42cd2025098f3e96011f6a4a6810d97522a9298b
Vladimir <v.khromov@datarockets.com>
2022-05-14 14:25:35 +0300

    Add CRUIDS

---
+++ src/modules/cargo/model/create-cargo-dto.interface.ts
@@ -0,0 +1,8 @@
+
+export interface CreateCargoDto {
+    weight: number;
+    length: number;
+    width: number;
+    height: number;
+    name: string;
+  }
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../shared/prisma/prisma.service';
 import { OrderStatus } from '@prisma/client';
+import { UpdateOrderDto } from './model/update-order-dto.interface';
+import { CreateOrderDto } from './model/create-order-dto.interface';
+import { LocationService } from '../location/location.service';
+import { CargoService } from '../cargo/cargo.service';
+import { CreateLocationDto } from '../location/model/create-location-dto.interface';
+import { CreateCargoDto } from '../cargo/model/create-cargo-dto.interface';
 
 @Injectable()
 export class OrderService {
-  constructor(private prisma: PrismaService) {}
 
-  async getNotApprovedOrders() {
-    const locationWhere = {
+  private locationWhere = {
       select: {
         home: true,
         city: {
@@ -22,8 +26,8 @@ export class OrderService {
         },
       },
     };
-    return this.prisma.order.findMany({
-      select: {
+
+  private orderInfo = {
         id: true,
         createdAt: true,
         updatedAt: true,
@@ -33,21 +37,111 @@ export class OrderService {
             lastName: true,
           },
         },
-        fromLocation: locationWhere,
-        toLocation: locationWhere,
+        fromLocation: this.locationWhere,
+        toLocation: this.locationWhere,
         status: true,
       },
-      where: {
-        status: OrderStatus.PENDING,
-      },
-    });
-  }
+
+    constructor(
+        private prismaService: PrismaService,
+        private locationService: LocationService,
+        private cargoService: CargoService,
+    ) {}
+
+    async createOrder(data: CreateOrderDto) {
+        const [fromLocationId, toLocationId] = await this.getLocationsIds(
+            data.fromLocation,
+            data.toLocation,
+        );
+        const order = await this.prismaService.order.create({
+            data: {
+                ownerId: data.userId,
+                toLocationId,
+                fromLocationId,
+            },
+        });
+        await this.createCargos(order.id, data.cargos);
+        return order;
+    }
+    async getNotApprovedOrders() {
+        return this.prismaService.order.findMany({
+            select: this.orderInfo,
+            where: {
+                status: OrderStatus.PENDING,
+            },
+        });
+    }
+
+    async getUserOrders(ownerId: number) {
+        return this.prismaService.order.findMany({
+            select: this.orderInfo,
+            where: { ownerId },
+        });
+    }
+
+    // TODO: implement filtration for orders
+    async getOrdersFilteredBy(status: OrderStatus, userId: number) {
+        return {
+            status,
+            userId,
+        };
+    }
+
+    async getOrder(id: number) {
+        return this.prismaService.order.findUnique({
+            select: this.orderInfo,
+            where: { id },
+        });
+    }
+
+    // To update cargos in the order use CargoController
+    async updateOrder(id: number, data: UpdateOrderDto) {
+        const [fromLocationId, toLocationId] = await this.getLocationsIds(
+            data.fromLocation,
+            data.toLocation,
+        );
+        return this.prismaService.order.update({
+            where: { id },
+            data: {
+                toLocationId,
+                fromLocationId,
+            },
+        });
+    }
 
   async approveOrder(id: number) {
-    return this.prisma.order.update({
+    return this.prismaService.order.update({
       select: { id: true },
       where: { id },
       data: { status: OrderStatus.APPROVED },
     });
   }
+
+    async declineOrder(id: number) {
+        return this.prismaService.order.update({
+            select: { id: true },
+            where: { id },
+            data: { status: OrderStatus.DECLINED },
+        });
+    }
+
+    private async getLocationsIds(
+        fromLocation: CreateLocationDto,
+        toLocation: CreateLocationDto,
+    ): Promise<number[]> {
+        return [
+            await this.locationService
+                .getOrCreateLocation(fromLocation)
+                .then((res) => res['id']),
+            await this.locationService
+                .getOrCreateLocation(toLocation)
+                .then((res) => res['id']),
+        ];
+    }
+
+    private async createCargos(orderId: number, cargos: CreateCargoDto[]) {
+        for (const cargo of cargos) {
+            await this.cargoService.createCargo(orderId, cargo);
+        }
+    }
 }
